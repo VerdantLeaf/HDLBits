5829bd3d84a5353a7dd6e050ddb3ce9575612eb1
VerdantLeaf <111800371+VerdantLeaf@users.noreply.github.com>
2025-01-21 20:19:33 -0500

    Completed KMaptoCircuit
    Completed combinational logic section

--- Circuits/Combinational Logic/kMapToCircuit.v
+++ Circuits/Combinational Logic/kMapToCircuit.v
@@ -71,4 +71,48 @@ module top_module (
 
 endmodule
 
-//
+// Karnaugh Map 2
+module top_module (
+    input [4:1] x,
+    output f ); 
+    
+    assign f = (~x[1] & x[3]) | (~x[2] & ~x[4]) | (x[2] & x[3] & x[4]);
+
+endmodule
+
+// K Map MUX
+module top_module (
+    input c,
+    input d,
+    output [3:0] mux_in
+); 
+
+    always @(*) begin
+        case({c ,d})
+            2'b00 : mux_in = 4'b0100;
+            2'b01 : mux_in = 4'b0001;
+            2'b10 : mux_in = 4'b0101;
+            2'b11 : mux_in = 4'b1001;
+        endcase
+    end
+    
+endmodule
+
+// Provided solution:
+// (much different than mine lol)
+module top_module (
+	input c,
+	input d,
+	output [3:0] mux_in
+);
+	
+	// After splitting the truth table into four columns,
+	// the rest of this question involves implementing logic functions
+	// using only multiplexers (no other gates).
+	// I will use the conditional operator for each 2-to-1 mux: (s ? a : b)
+	assign mux_in[0] = c ? 1 : d;          // 1 mux:   c|d
+	assign mux_in[1] = 0;                  // No muxes:  0
+	assign mux_in[2] = d ? 0 : 1;          // 1 mux:    ~d
+	assign mux_in[3] = c ? d : 0;          // 1 mux:   c&d
+	
+endmodule
